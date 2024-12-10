ba6492853b3e00afa4ca966f6b909ad54c51ca0c
Oscar Poco <123397536+Oscarpoco@users.noreply.github.com>
2024-12-10 10:01:54 -0800

    I have created the user schema

---
+++ server/models/user.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+import bcrypt from 'bcrypt';
+
+const userSchema = new mongoose.Schema({
+
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    phone: { type: Number, required: true },
+});
+
+userSchema.methods.matchPassword = async function (enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password);
+}
+
+const User = mongoose.model("Users", userSchema);
